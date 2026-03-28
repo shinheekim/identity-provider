1dc158bc5eae6f9985ea957084f5783c48653ea4
shinheekim <dev.shinheekim@gmail.com>
2026-03-28 22:21:18 +0900

    chore: spring boot 로그인 서비스 초기 프로젝트 구성

---
+++ src/test/java/com/pj/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
